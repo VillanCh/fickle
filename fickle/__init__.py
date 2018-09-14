4ac0432603485e60ecf6559fe15544c73cf503e1
v1ll4n <v1ll4n@villanch.top>
2018-09-14 17:19:14 +0800

    export param types

--- fickle/__init__.py
+++ fickle/__init__.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # coding:utf-8
 from .core import FickleRequest
+from .query_manager import FickleQueryParam
+from .post_manager import FicklePostParam, FickleJsonParam
+from .header_manager import FickleCookiesParam
 
 __all__ = [
-    "FickleRequest"
+    "FickleRequest", "FicklePostParam", "FickleQueryParam",
+    "FickleCookiesParam", "FickleJsonParam"
 ]
